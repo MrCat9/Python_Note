bd2ac2a70a118b0244beb131f84a71049503456d
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2018-09-29 10:21:29 +0800

    Update 16_3_多进程_子进程.py

--- 16_3_多进程_子进程.py
+++ 16_3_多进程_子进程.py
@@ -27,3 +27,39 @@ Aliases:  www.python.org
 Exit code: 0
 
 """
+
+
+#
+print("================================")
+#
+
+
+# 如果子进程还需要输入，则可以通过communicate()方法输入：
+print('$ nslookup')
+p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, err = p.communicate(b'set q=mx\npython.org\nexit\n')  # b:bytes
+print(output.decode('GB2312'))
+print('Exit code:', p.returncode)
+
+
+'''
+上面的代码相当于在命令行执行命令nslookup，然后手动输入：
+set q=mx
+python.org
+exit
+'''
+
+
+"""
+$ nslookup
+默认服务器:  cache-fz.fj133165.com
+Address:  58.22.96.66
+
+> > 服务器:  cache-fz.fj133165.com
+Address:  58.22.96.66
+
+python.org	MX preference = 50, mail exchanger = mail.python.org
+> 
+Exit code: 0
+
+"""
