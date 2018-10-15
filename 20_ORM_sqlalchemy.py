3bf69a4ad2e2def14ce7a98c357378c428827e57
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2018-10-15 10:55:53 +0800

    Create 20_ORM_sqlalchemy.py

---
+++ 20_ORM_sqlalchemy.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# 摘自 https://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/0014021031294178f993c85204e4d1b81ab032070641ce5000
+
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# 创建对象的基类:
+Base = declarative_base()
+
+
+# 定义User对象:
+class User(Base):
+    # 表的名字:
+    __tablename__ = 'users'
+
+    # 表的结构:
+    id = Column(String(20), primary_key=True)
+    name = Column(String(20))
+
+
+# 初始化数据库连接:
+engine = create_engine('mysql+pymysql://root:123456@localhost:3306/pymysql_test01')
+# '数据库类型+数据库驱动名称://用户名:口令@机器地址:端口号/数据库名'
+
+# 创建数据表
+Base.metadata.create_all(engine)
+
+# 创建DBSession类型:
+DBSession = sessionmaker(bind=engine)
+
+
+# 插入数据
+# # 创建session对象:
+# session = DBSession()
+# # 创建新User对象:
+# new_user = User(id='6', name='Bob2')
+# # 添加到session:
+# session.add(new_user)
+# # 提交即保存到数据库:
+# session.commit()
+# # 关闭session:
+# session.close()
+
+
+# 查询数据
+# 创建Session:
+session = DBSession()
+# 创建Query查询，filter是where条件，最后调用one()返回唯一行，如果调用all()则返回所有行:
+user = session.query(User).filter(User.id == '5').one()
+# 打印类型和对象的name属性:
+print('type:', type(user))
+print('name:', user.name)
+# 关闭Session:
+session.close()
