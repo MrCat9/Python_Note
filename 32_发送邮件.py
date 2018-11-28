03dd52c8b21dd6b2bfed9ffa92cf912a1f9ffb7b
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2018-11-28 14:51:19 +0800

    Update 32_发送邮件.py

--- 32_发送邮件.py
+++ 32_发送邮件.py
@@ -7,7 +7,7 @@ from email.header import Header
 
 # 第三方 SMTP 服务
 mail_host = "smtp.sina.com"  # 设置服务器
-mail_user = 111sina.com"  # 用户名
+mail_user = "111sina.com"  # 用户名
 mail_pass = "···"  # 口令
 
 sender = '111@sina.com'
