8bab4848af32a12f4951c6d973cc1549f12309ea
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2019-01-02 11:06:48 +0800

    Update 11_对dict的list去重.py

--- 11_对dict的list去重.py
+++ 11_对dict的list去重.py
@@ -2,13 +2,37 @@
 # 根据dict中的某一key的value，对dict的list去重，value相同视为重复
 
 
+# def dict_list_duplicate(dict_list, duplicate_key):
+#     temp_list = []
+#     new_dict_list = []
+#     for dict_ in dict_list:
+#         value = dict_[duplicate_key]
+#         if value not in temp_list:
+#             temp_list.append(value)
+#             new_dict_list.append(dict_)
+#     return new_dict_list
+
+
+# def dict_list_duplicate(dict_list, duplicate_key):
+#     temp_set = set()
+#     new_dict_list = []
+#     for dict_ in dict_list:
+#         value = dict_[duplicate_key]
+#         if value not in temp_set:
+#             temp_set.add(value)
+#             new_dict_list.append(dict_)
+#     return new_dict_list
+
+
 def dict_list_duplicate(dict_list, duplicate_key):
-    temp_list = []
+    temp_set = set()
     new_dict_list = []
     for dict_ in dict_list:
         value = dict_[duplicate_key]
-        if value not in temp_list:
-            temp_list.append(value)
+        old_set_len = len(temp_set)
+        temp_set.add(value)
+        new_set_len = len(temp_set)
+        if new_set_len > old_set_len:
             new_dict_list.append(dict_)
     return new_dict_list
 
