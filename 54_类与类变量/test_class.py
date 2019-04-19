86713383aa857c7647eb3a94776c477b0c4d066c
lins15 <lins15@chinaunicom.cn>
2019-04-19 19:44:42 +0800

    update

--- 54_类与类变量/test_class.py
+++ 54_类与类变量/test_class.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-_test_class_var = '_test_class_var'
-__test_class_var = '__test_class_var'
+_py_var = '_py_var'
+__py_var = '__py_var'
+py_var = 'py_var'
 
 
 class TestClass:  # class TestClass(object):  # 括号里是 TestClass 类要继承的父类
-    _class_var1 = '_class_var1'
+    _class_var1 = '_class_var1'  # 类变量
     __class_var2 = '__class_var2'
     class_var = 'class_var'
 
@@ -13,9 +14,16 @@ class TestClass:  # class TestClass(object):  # 括号里是 TestClass 类要继
         print('执行init方法')
         print(arg1)
         print(arg2)
+        self._instance_var1 = '_instance_var1'  # 实例变量
+        self.__instance_var2 = '__instance_var2'
+        self.instance_var = 'instance_var'
 
-    def say(self, text):
+    @staticmethod
+    def say_staticmethod(text):  # 类方法  # 没有用到 self 的话建议定义成类方法
         print(text)
+
+    def say(self, text):  # 实例方法  # 要用到 self 的话建议定义成实例方法
+        print(text, self)
 
     def get_class_var1(self):
         print(self._class_var1)
@@ -24,6 +32,24 @@ class TestClass:  # class TestClass(object):  # 括号里是 TestClass 类要继
     def get_class_var2(self):
         print(self.__class_var2)
         return self.__class_var2
+
+    def get_class_var(self):
+        print(self.class_var)
+        return self.class_var
+
+    #
+
+    def get_instance_var1(self):
+        print(self._instance_var1)
+        return self._instance_var1
+
+    def get_instance_var2(self):
+        print(self.__instance_var2)
+        return self.__instance_var2
+
+    def get_instance_var(self):
+        print(self.instance_var)
+        return self.instance_var
 
 
 
--- 54_类与类变量/use_test_class.py
+++ 54_类与类变量/use_test_class.py
@@ -1,25 +1,58 @@
 # -*- coding: utf-8 -*-
 
 from test_class import TestClass
-from test_class import _test_class_var
-from test_class import __test_class_var
+from test_class import _py_var
+from test_class import __py_var
+from test_class import py_var
 
 
 if __name__ == '__main__':
+    print(_py_var)  # _py_var
+    print(__py_var)  # __py_var
+    print(py_var)  # py_var
+
+    #
+    
+    # 类调用类变量
+    print(TestClass._class_var1)  # _class_var1
+    # print(TestClass.__class_var2)  # 报错
+    print(TestClass.class_var)  # class_var
+    
+    # 类调用类方法
+    TestClass.say_staticmethod('class_hello')  # hello  # 类直接调用类方法时，需要给类方法传参数
+    # 类调用实例方法
+    TestClass.say(None, 'class_hello')  # hello  # 类直接调用实例方法时，也需要给 self 传参数
+
+    #
+
     test = TestClass('arg1', 'arg2')
     '''
     执行init方法
     arg1
     arg2
     '''
+    
+    # 实例调用类变量
     print(test._class_var1)  # _class_var1
     # print(test.__class_var2)  # 报错
     print(test.class_var)  # class_var
+    
+    # 实例调用实例变量
+    print(test._instance_var1)  # _instance_var1
+    # print(test.__instance_var2)  # 报错
+    print(test.instance_var)  # instance_var
 
-    print(_test_class_var)  # _test_class_var
-    print(__test_class_var)  # __test_class_var
+    # 实例调用类方法
+    test.say_staticmethod('hello')  # hello  # 类的实例对象调用类方法（静态方法）时，不会将实例本身传给第一个参数
+    # 实例调用实例方法
+    test.say('hello')  # hello  # 类的实例对象调用实例方法时，会将对象本身自动传给第一个参数（一般实例方法的第一个参数是 self）
 
-    test.say('hello')  # hello
     test.get_class_var1()  # _class_var1
     test.get_class_var2()  # __class_var2
+    test.get_class_var()  # class_var
+    test.get_instance_var1()  # _instance_var1
+    test.get_instance_var2()  # __instance_var2
+    test.get_instance_var()  # instance_var
+
+
 
