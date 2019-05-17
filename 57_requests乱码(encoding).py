0dafec57050339deab2c68bded6d1c105ecb6652
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2019-05-17 17:20:03 +0800

    Update 57_requests乱码(encoding).py

--- 57_requests乱码(encoding).py
+++ 57_requests乱码(encoding).py
@@ -5,6 +5,7 @@
 import requests
 
 
+# ================================ 请求网页 ================================
 url = 'https://www.chinanews.com/gn/2019/05-16/8837888.shtml'
 params = {}
 headers = {}
@@ -12,7 +13,10 @@ proxies = {}
 timeout = 10
 response = requests.get(url=url, params=params, headers=headers, proxies=proxies, timeout=timeout)
 html_str = response.text
+# ==========================================================================
 print(html_str)  # 编码前
+
+# ================================ 设置网页编码 ================================
 charset = requests.utils.get_encodings_from_content(html_str)  # 从html的meta中抽取
 '''
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -21,5 +25,6 @@ if not charset:
     charset = response.apparent_encoding  # apparent_encoding 返回真实编码  # 由程序分析出编码
 response.encoding = charset
 html_str = response.text
+# ==============================================================================
 print(html_str)  # 编码后
 
