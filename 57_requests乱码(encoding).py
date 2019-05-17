e1d0aa25935e8705102163c32bbc074572eeba59
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2019-05-17 10:36:30 +0800

    Create 57_requests乱码(encoding).py

---
+++ 57_requests乱码(encoding).py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# https://www.cnblogs.com/bw13/p/6549248.html
+
+
+import requests
+
+
+url = 'https://www.chinanews.com/gn/2019/05-16/8837888.shtml'
+params = {}
+headers = {}
+proxies = {}
+timeout = 10
+response = requests.get(url=url, params=params, headers=headers, proxies=proxies, timeout=timeout)
+html_str = response.text
+print(html_str)  # 编码前
+charset = requests.utils.get_encodings_from_content(html_str)  # 从html的meta中抽取
+'''
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+'''
+if not charset:
+    charset = response.apparent_encoding  # apparent_encoding 返回真实编码  # 由程序分析出编码
+response.encoding = charset
+html_str = response.text
+print(html_str)  # 编码后
+
