9c2eee0b4941de01c6d96762cfb7445770715e7d
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2019-05-17 10:47:57 +0800

    Update 13_用re去除所有html标签，保留标签里面的内容.py

--- 13_用re去除所有html标签，保留标签里面的内容.py
+++ 13_用re去除所有html标签，保留标签里面的内容.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # 摘自 https://www.jb51.net/article/65497.htm
+# https://blog.csdn.net/weixin_41972401/article/details/79991591
 
 import re
 html='<a href="//www.jb51.net">脚本之家</a>,Python学习！'
