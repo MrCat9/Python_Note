5262e617fb2f5fb9734bf6677e9d370ba815eba5
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2019-07-04 13:52:41 +0800

    Create 65_captcha_mark.py

---
+++ 65_captcha_mark.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# 验证码手动标记
+
+
+import requests
+# import time
+from PIL import Image
+import matplotlib.pyplot as plt
+import shutil
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+    # 'User-Agent': get_ua()  # 获取随机 UA头
+}
+
+
+if __name__ == '__main__':
+    url = ''
+    for i in range(1000):
+        print(i)
+        # time.sleep(3)
+
+        # ======== 下载图片 ========
+        r = requests.get(url=url, headers=headers)
+        # with open('png_data/{}.png'.format(i), 'wb') as f:
+        with open('png_data/temp.png', 'wb') as f:
+            f.write(r.content)
+            f.close()
+        # ==========================
+
+        # ======== 显示图片 ========
+        img = Image.open('png_data/temp.png')
+        plt.figure('temp')
+        plt.imshow(img)
+        plt.show()
+        # ==========================
+
+        # ======== 输入验证码 ========
+        name = input('输入验证码：')
+        # ============================
+
+        # ======== 文件重命名 ========
+        shutil.copy('png_data/temp.png', 'png_data/{}.png'.format(name))
+        # ============================
